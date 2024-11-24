99bdb0a31d1bf241af21f52939abcb19147d862b
mengzhuou <mou30@gatech.edu>
2024-11-24 12:48:24 -0500

    update

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -7,7 +7,7 @@ const corsOptions = {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
-            callback(new Error('Not allowed by LMCFilm CORS'))
+            callback(new Error('Not allowed by JobLinkHub CORS'))
         }
     },
     credentials: true,
