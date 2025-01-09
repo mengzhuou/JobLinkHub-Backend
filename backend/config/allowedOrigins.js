c3bb435696a3fa3b8442f6bf3a927980e0aada12
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2025-01-08 19:34:36 -0500

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,10 +1,8 @@
 // only these url are allowed to access our api, otherwise our api is open to the public
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://www.lmcfilms.lmc.gatech.edu/',
-    'https://lmcfilms.lmc.gatech.edu/',
     'https://joblinkhub.netlify.app',
-    'https://job-link-hub-c5afaf8cad65.herokuapp.com'
+    'https://joblinkhub-backend.onrender.com'
 ]
 
 module.exports = allowedOrigins
